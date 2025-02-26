8de8c854fcb011e0a0a549d8ecfc9c9f20d3e261
Audyari Wiyono <audy123ari@gmail.com>
2025-02-26 13:44:57 +0700

    membuat function

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/Audyari/app-say-hello
+
+go 1.24.0
+
+require github.com/Audyari/go-say-hello v1.5.0 // indirect
+
+
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import go_say_hello "github.com/Audyari/go-say-hello"
+
+// go run main.go
+
+func main() {
+	println(go_say_hello.SayHello())
+	println(go_say_hello.SayGoodbye())
+}
