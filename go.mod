ffb349a859bdec4cd698c451b32575559a762913
Audyari Wiyono <audy123ari@gmail.com>
2025-02-26 14:23:06 +0700

    update versi 2

--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/Audyari/app-say-hello
 
 go 1.24.0
 
-require github.com/Audyari/go-say-hello v1.5.0 // indirect
-
-
+require github.com/Audyari/go-say-hello/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
-import go_say_hello "github.com/Audyari/go-say-hello"
+import go_say_hello "github.com/Audyari/go-say-hello/v2"
 
 // go run main.go
 
 func main() {
-	println(go_say_hello.SayHello())
-	println(go_say_hello.SayGoodbye())
+	println(go_say_hello.SayHello("Audyari w"))
+	println(go_say_hello.SayGoodbye("Asep"))
 }
